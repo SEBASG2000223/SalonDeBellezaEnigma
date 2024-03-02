a65ad2638ad0c1413f6a895f2c6290ee3b4d6bff
sebas <sebas@El_Bicho>
2024-03-02 15:30:11 -0600

    Creacion de Proyecto

---
+++ src/main/java/com/proyectoEnigma/ProyectoEnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoEnigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEnigmaApplication.class, args);
+	}
+
+}
