4602b87a8c2e0ff3a3075f680cc6adb71d604e46
tomas <tomas@TomasLaptop>
2024-03-04 11:10:56 -0600

    Solución error Sucursales tipo Conozcanos

--- src/main/java/com/proyectoEnigma/serviceImpl/SucursalesServiceImpl.java
+++ src/main/java/com/proyectoEnigma/serviceImpl/SucursalesServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.proyectoEnigma.dao.ConozcanosDao;
+import com.proyectoEnigma.dao.SucursalesDao;
 
 @Service
 
