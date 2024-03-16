a142dbdb6ee1b3b811bcd0c2485c7a7c231b0c23
sebas <sebas@DESKTOP-NEMJAN6>
2024-03-15 21:51:37 -0600

    Formato arreglo

--- src/main/resources/SALONNN.sql
+++ src/main/resources/SALONNN.sql
@@ -46,29 +46,32 @@ CREATE TABLE Conozcanos (
     activo BOOLEAN
 );
 
-
 INSERT INTO Accesorios (id_accesorios, nombre_accesorio, descripcion, ruta_imagen, activo) VALUES 
-('1','Silla Profesional', 'Ideal para brindar comodidad y estilo a tus clientes.', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fsilla.jpg?alt=media&token=05eaf633-2201-4d5d-8588-72cc19a83595', true), 
-('2','Secadora Profesional', 'Potente y eficiente para obtener resultados profesionales.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fsecadora.jpg?alt=media&token=c9965501-8860-4958-9940-e803ddf3309b', true),
-('3','Tijeras para Cortar Cabello','Precisas y ergonómicas para lograr cortes perfectos.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FTIJERAS.jpg?alt=media&token=3bc7fb9c-de3a-4632-b832-6335302d73f5',true),
-('4','Plancha Profesional','Consigue un cabello liso y brillante con esta plancha de alta calidad','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fplancha%20cabello.png?alt=media&token=2b5d2089-4fbd-4af1-b2c8-0941b9b34ca0',true),
-('5','Kit de Peines','Variedad de peines profesionales para todo tipo de estilos y cortes.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fkit%20de%20peines.png?alt=media&token=b859775b-21bc-4d48-9b06-70ce4cba5287',true),
-('6','Capa Profesional','Mantén a tus clientes cómodos y protegidos durante sus servicios.', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fcapa.png?alt=media&token=02fbf9f7-b828-4455-8e9c-b2150f6511e1', false);
+('1','Silla Profesional', 'Ideal para brindar comodidad y estilo a tus clientes.', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fsilla.png?alt=media&token=ecd78915-1ad0-428c-8fac-bed2303a9b0c', true), 
+('2','Secadora Profesional', 'Potente y eficiente para obtener resultados profesionales.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fsecador.png?alt=media&token=0b30a518-51c1-4618-8135-943cf197bd38', true),
+('3','Tijeras para Cortar Cabello','Precisas y ergonómicas para lograr cortes perfectos.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Ftijeras.png?alt=media&token=b6391486-a0d0-4337-835b-732bc02b72d1',true),
+('4','Plancha Profesional','Consigue un cabello liso y brillante con esta plancha de alta calidad','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fplancha.png?alt=media&token=380ed9af-52c7-43cf-95f9-316258e4aeff',true),
+('5','Kit de Peines','Variedad de peines profesionales para todo tipo de estilos y cortes.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fpeine.png?alt=media&token=583ba4d7-67a1-44de-a072-735e62a626e8',true),
+('6','Capa Profesional','Mantén a tus clientes cómodos y protegidos durante sus servicios.', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fcapa.png?alt=media&token=9cf8be5b-4232-4f88-b22a-f1dff0769eed', false),
+('7','Espejo de Maquillaje con Luz','Ideal para aplicar maquillaje con precisión gracias a su iluminación LED.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fespejo.png?alt=media&token=de1138a7-d564-436b-bad2-f639d3711a21', true),
+('8','Cepillo Profesional','Perfecto para desenredar y dar volumen al cabello.','https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FAccesorios%2Fcepillo.png?alt=media&token=033a34aa-e1bc-460f-82dc-822d47ae6a5e', true);
 
 INSERT INTO Servicios (id_servicios, nombre_servicio, descripcion, ruta_imagen, activo)  
 VALUES      
-    (1, 'corte', 'Dar forma, recortar y estilizar el cabello', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fcorte.png?alt=media&token=460421ae-fe22-4f19-bf10-81a7391ec992', true),     
-    (2, 'Queratina', 'Tratamiento para el cabello que ayuda a suavizar, alisar y reparar el cabello dañado', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Fqueratina.png?alt=media&token=d53e8e53-98ce-441c-aaa1-8d2e27ec9dce', true),     
-    (3, 'Tinte', 'Proceso de aplicar color artificial al cabello para cambiar su tono', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2Ftinte.png?alt=media&token=9ea294ac-79db-44bf-b3ba-bbafaf41c43d', true),     
-    (4, 'Manicura', 'Servicio de cuidado de las uñas de las manos, que incluye limado, recorte, pulido y aplicación de esmalte', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FManicura.png?alt=media&token=57030465-3651-4b1d-97c8-91e81e1f1f94', true),     
-    (5, 'Pedicura', 'Servicio de cuidado de las uñas de los pies, que incluye limado, recorte, pulido y aplicación de esmalte', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FPedicura.png?alt=media&token=e8e3deba-b23c-4206-8d89-4c9cbee0de1f', true),     
-    (6, 'Gel X', 'Método de aplicación de uñas de gel que utiliza puntas preformadas y gel específico para una apariencia y durabilidad mejoradas', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FGelX.png?alt=media&token=cc33b05e-0f3f-4bdf-a5f9-d826d7a876b4', true);
+    (1, 'Corte de cabello', 'Dar forma y estilizar el cabello con los mejores productos y estilistas de la zona', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fcorte.png?alt=media&token=90b21f02-a34b-4cb8-a699-16f3d7973d44', true),     
+    (2, 'Queratina', 'Tratamiento para suavizar y reparar el cabello dañado. En tan solo unas hora tu cabello lucira como nuevo', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fqueratina.png?alt=media&token=8fe9b9e6-b8db-4ea2-b5ef-7014c5060e77', true),     
+    (3, 'Tinte', 'Proceso para cambiar el tono del cabello y cambiarlo al tono o color de tu preferencia', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Ftinte.png?alt=media&token=cc33ab67-48f3-4466-8d94-faca4b6e6e4a', true),     
+    (4, 'Manicura', 'Servicio de cuidado, pintado y esmaltado de las uñas de las manos, con los mejores productos', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fmanicura.png?alt=media&token=abcd9f7e-04dd-4c0d-ac53-89d49ec0a730', true),     
+    (5, 'Pedicura', 'Servicio de cuidado de las uñas de los pies con los mejores productos y maquinas del mercado', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fpedicura.png?alt=media&token=ddbfaeb7-197a-4f91-87ae-f844929c3602', true),
+    (6, 'Gel X', 'Método de aplicación de uñas de gel con una duración grantizada ya que se hace con los mejores productos', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fgell%20x.png?alt=media&token=a1fb644d-366e-4894-9d92-f8136578fe8b', true),
+    (7, 'Tratamiento capilar de hidratación profunda', 'Tratamiento que proporciona hidratación intensiva al cabello', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fhidratacion.png?alt=media&token=4be18df3-792e-4b1d-a1c5-e2bed13d3eb4', true),     
+    (8, 'Maquillaje profesional', 'Servicio de maquillaje realizado por los mejores estilistas de la zona y con los mejores productos', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FServicios%2Fmaquillaje.png?alt=media&token=e91cb533-c6bf-47ab-a6f1-410a37389210', true);
 
 
 INSERT INTO Sucursales (id_sucursales, nombre_sucursal, descripcion, ruta_imagen, activo) 
 VALUES 
     (1, 'Zapote', '500mts al norte de multiplaza del este', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FZapote.png?alt=media&token=ef96ad12-8a9a-469e-8bd8-4a8ebf64ef3d', true),
-    (2, 'Desamparados', '25mts sur y 100 mts este de multicentro desamparados', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FDesamparados.png?alt=media&token=2d4d47b9-a824-43fb-bbea-25a89f1172f1', true),
+    (2, 'Desamparados', '100 mts este del multicentro, desamparados', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FDesamparados.png?alt=media&token=2d4d47b9-a824-43fb-bbea-25a89f1172f1', true),
     (3, 'Guadalupe', '200mts oeste del Centro comercial de guadalupe', 'https://firebasestorage.googleapis.com/v0/b/enigmasalon-4beba.appspot.com/o/Salon%2FGuadalupe.png?alt=media&token=2acafb4e-26e3-4ef1-b628-b62687c9e139', true);
 
 drop database SalonEnigma;
